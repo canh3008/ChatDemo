df94e3842ca83f12b6d9d79ad39ef990c40823f5
Duc Canh <tranduccanh3008@gmail.com>
2023-04-17 09:52:06 +0700

    setup pod file

---
+++ ChatDemo/ViewControllers/Authentications/Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  ChatDemo
+//
+//  Created by Duc Canh on 17/04/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ ChatDemo/ViewControllers/Authentications/Register/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  ChatDemo
+//
+//  Created by Duc Canh on 17/04/2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
