c8a2b0232f56f31c6b484e6cba3169066ffda97d
Duc Canh <tranduccanh3008@gmail.com>
2023-04-18 11:48:59 +0700

    create validator service

---
+++ ChatDemo/Managers/Validator.swift
@@ -0,0 +1,55 @@
+//
+//  Validator.swift
+//  ChatDemo
+//
+//  Created by Duc Canh on 18/04/2023.
+//
+
+import Foundation
+import RxSwift
+
+enum ValidationResult<Failed> {
+    case success
+    case failed(Failed)
+}
+
+protocol ValidateFeature {
+    associatedtype ResultValue
+    associatedtype Value
+    func checkValidate(with type: ValidationType<Value>) -> Observable<ValidationResult<ResultValue>>
+}
+
+enum ValidationType<Value> {
+    case email(Value)
+    case password(Value)
+}
+
+enum MessageErrorDefault: String {
+    case notDefined = "Not defined object"
+    case email = "Email Error"
+    case emailEmpty = "Please enter the email"
+    case password = "Password Error"
+    case passwordEmpty = "Please enter the password"
+}
+
+class Validator: ValidateFeature {
+    typealias ResultValue = String
+    func checkValidate(with type: ValidationType<String>) -> Observable<ValidationResult<ResultValue>> {
+        Observable.create { [weak self] observer -> Disposable in
+            switch type {
+            case .email(let email):
+                observer.onNext(self?.getEmailValidationResult(email) ?? .failed(MessageErrorDefault.notDefined.rawValue))
+            case .password:
+                observer.onNext(.success)
+            }
+            return Disposables.create()
+        }
+    }
+
+    func getEmailValidationResult(_ email: String) -> ValidationResult<ResultValue> {
+        guard !email.isEmpty else {
+            return .failed(MessageErrorDefault.emailEmpty.rawValue)
+        }
+        return email.isValidEmail ? .success : .failed(MessageErrorDefault.email.rawValue)
+    }
+}
--- ChatDemo/ViewControllers/Authentications/Login/LoginViewModel.swift
+++ ChatDemo/ViewControllers/Authentications/Login/LoginViewModel.swift
@@ -11,6 +11,28 @@ import RxSwift
 
 class LoginViewModel: BaseViewModel, ViewModelTransformable {
 
+    private let validator: Validator
+
+    init(validator: Validator = Validator()) {
+        self.validator = validator
+    }
+
+    func transform(input: Input) -> Output {
+
+        let validateEmail = input.email
+            .flatMapLatest { [weak self] email -> Observable<ValidationResult<Validator.ResultValue>> in
+                guard let self = self else {
+                    return .empty()
+                }
+                return self.validator.checkValidate(with: .email(email))
+            }
+
+        
+        return Output()
+    }
+}
+
+extension LoginViewModel {
     struct Input {
         let email: Observable<String>
         let password: Observable<String>
@@ -19,9 +41,5 @@ class LoginViewModel: BaseViewModel, ViewModelTransformable {
 
     struct Output {
 
-    }
-
-    func transform(input: Input) -> Output {
-        return Output()
     }
 }
